4d87504af3d4e855206a5f9358bd6c20b6f7241d
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2023-03-15 16:11:15 -0300

    Refatoração na home

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { Card } from "../../components/card"
 import { Header } from "../../components/header"
-import "./style.css"
+import Style from "./style.module.css"
 
 export const Home = () => {
 
@@ -10,38 +10,30 @@ export const Home = () => {
     return (
         <>
             <Header />
-            <div className="cards">
-                {
-                    (role === "gestor")
-
-                        ?
-
-                        <>
-                            <Card titulo="Solicitação de Férias" icone="fas fa-plane-departure" botao="Solicitar" link="/solicitar" />
-                            <Card titulo="Minhas Solicitações" icone="fa-regular fa-file-lines" botao="Solicitar" link="/minhas-solicitacoes" />
-                            <Card titulo="Dashboard" icone="fa-solid fa-chart-pie" botao="Visualizar" link="/dashboard" />
-                        </>
-
-                        :
-
-                        (role === "funcionario")
-
+            <div className={Style.containerCards}>
+                <div className={Style.cards}>
+                    {
+                        (role === "gestor")
                             ?
-
                             <>
                                 <Card titulo="Solicitação de Férias" icone="fas fa-plane-departure" botao="Solicitar" link="/solicitar" />
                                 <Card titulo="Minhas Solicitações" icone="fa-regular fa-file-lines" botao="Solicitar" link="/minhas-solicitacoes" />
+                                <Card titulo="Dashboard" icone="fa-solid fa-chart-pie" botao="Visualizar" link="/dashboard" />
                             </>
-
                             :
-
-                            <>
-                                <Card titulo="Cadastrar Colaborador" icone="fa-regular fa-user" botao="Cadastrar" link="/cadastro" />
-                                <Card titulo="Listar Colaboradores" icone="fa-solid fa-users" botao="Visualizar" link="/colaboradores" />
-                            </>
-
-                }
-
+                            (role === "funcionario")
+                                ?
+                                <>
+                                    <Card titulo="Solicitação de Férias" icone="fas fa-plane-departure" botao="Solicitar" link="/solicitar" />
+                                    <Card titulo="Minhas Solicitações" icone="fa-regular fa-file-lines" botao="Solicitar" link="/minhas-solicitacoes" />
+                                </>
+                                :
+                                <>
+                                    <Card titulo="Cadastrar Colaborador" icone="fa-regular fa-user" botao="Cadastrar" link="/cadastro" />
+                                    <Card titulo="Listar Colaboradores" icone="fa-solid fa-users" botao="Visualizar" link="/colaboradores" />
+                                </>
+                    }
+                </div>
             </div>
         </>
     )
