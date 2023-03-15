4d8a977fa8a28e9e11f9dcd72d74b55fd28e0d20
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2023-03-15 16:00:20 -0300

    Refatoração no css do header

--- frontend/src/components/header/index.js
+++ frontend/src/components/header/index.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom"
 import { Notificacoes } from "../notificacoes";
-import "./style.css"
+import Style from "./style.module.css"
+import Logo from "../../assets/imgs/logo.png"
 
 export const Header = () => {
 
@@ -33,25 +34,27 @@ export const Header = () => {
 
     return (
         <header>
-            <div className="logo">
-                <Link to="/home">QQFérias</Link>
+            <div className={Style.logo}>
+                <Link to="/home"><img src={Logo} alt="Logo" /></Link>
             </div>
-            <div className="opcoes">
-                {(numNotific > 0) ? <span className="count">{numNotific}</span> : null}
+            <div className={Style.opcoes}>
+                {(numNotific > 0) ? <span className={Style.count}>{numNotific}</span> : null}
                 <span onClick={mostrarNotificacao}><i className="fa-regular fa-bell" title="notificação"></i></span>
                 <Link to="/perfil"><i className="fa-regular fa-user" title="perfil"></i></Link>
                 <Link to="/" onClick={() => { dispatch({ type: 'LOGOUT' }) }}><i className="fa-solid fa-door-open" title="sair"></i>
                 </Link>
-                <div className={`notificacoes ${visivel}`} >
-                    <div className="topo">
-                        <span>Notificações</span>
-                    </div>
-                    {notificacoes.map((notif, index) => <Notificacoes key={index} texto={notif.texto} />)}
-                    <div className="fundo">
-                        <span>Visualizar Tudo</span>
+                <div className={`containerNotificacao ${visivel}`}>
+                    <div className={Style.notificacoes} >
+                        <div className={Style.topo}>
+                            <span>Notificações</span>
+                        </div>
+                        {notificacoes.map((notif, index) => <Notificacoes key={index} texto={notif.texto} />)}
+                        <div className={Style.fundo}>
+                            <span>Visualizar Tudo</span>
+                        </div>
                     </div>
                 </div>
             </div>
-        </header>
+        </header >
     )
 }
