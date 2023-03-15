2b89716c26635921a27dc3408525896f291569a7
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2023-03-15 15:30:26 -0300

    Adicionado css modules e refatoração do login

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,7 +1,8 @@
 import { useState } from "react"
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from "react-redux";
-import "./style.css"
+import Style from "./style.module.css"
+import Logo from "../../assets/imgs/logo.png"
 
 export const Login = () => {
 
@@ -22,7 +23,7 @@ export const Login = () => {
     }
 
     return (
-        <div className="container-principal">
+        <div className={Style.containerPrincipal}>
 
             {msg === true ?
                 (<div className="alert alert-warning w-25 mx-auto" role="alert">
@@ -32,11 +33,18 @@ export const Login = () => {
                 null
             }
 
-            <h1>QQFérias</h1>
-            <div className="container-login">
-                <input type="text" placeholder="Login" value={login} onChange={(evt) => setLogin(evt.target.value)} />
-                <input type="password" placeholder="Senha" value={senha} onChange={(evt) => setSenha(evt.target.value)} />
-                <button to="/home" id="entrar" onClick={redirecionar}>Entrar</button>
+            <div className={Style.containerLogin}>
+                <div className={Style.containerLogo}>
+                    <img className={Style.logo} src={Logo} alt="Logo" />
+                    <h1>Férias</h1>
+                </div>
+                <div className={Style.loginInput}>
+                    <i className="fa-regular fa-user"></i><input type="text" placeholder="Login" value={login} onChange={(evt) => setLogin(evt.target.value)} />
+                </div>
+                <div className={Style.loginInput}>
+                    <i className="fa-solid fa-lock"></i><input type="password" placeholder="Senha" value={senha} onChange={(evt) => setSenha(evt.target.value)} />
+                </div>
+                <button to="/home" className={Style.botao} onClick={redirecionar}>Entrar</button>
             </div>
         </div >
     )
