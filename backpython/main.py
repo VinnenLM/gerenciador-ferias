5a5fc89ac4bd841c93996995d53f32bc50e88cdf
Vinícius de Lima Mendonça <vinicius.mendoncaa@gmail.com>
2023-03-31 09:25:07 -0300

    Criada rota para envio de relatórios

--- backpython/main.py
+++ backpython/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 import requests
 import smtplib
@@ -6,7 +6,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 from fastapi.middleware.cors import CORSMiddleware
-
+import pandas as pd
+import json as js
 
 class ObjetoNotificacao(BaseModel):
     idWorkplace: str | None = None
@@ -31,7 +32,7 @@ app.add_middleware(
 @app.post("/enviarNotificacao")
 async def enviar_notificacao(obj: ObjetoNotificacao):
     try:
-        print(obj)
+
         url = "https://graph.facebook.com/v4.0/me/messages"
 
         token='DQVJzbi1raTJheGV6Ump4NE5RYUVUTTZAwNGx5dmdJOE00VTRDUHR6RjVxdTdYOHYtaTcxQW9pVHBhOVhiTW9BQkdrU3A3dUdpOU1EQ0ZAfeU1HaHloSGZAuVGFNQkctSlhMVjJHUHZAMcU82OG90dG5heE9uLW9YSTlOdExBWU8zVnNybm5vMU5kMEFoalBDRUZAvNlkyVG5PSlhqWTEtaWZAyazJ3di0yTTJ1aEdmTG5JMTRTd0tUWFlpb3ctSWFDNndpUWh3OXdn'
@@ -59,19 +60,19 @@ async def enviar_notificacao(obj: ObjetoNotificacao):
         r = requests.post(url, json=data, headers=headers)
 
         return r.status_code
+    
     except:
+
         return { "message": "Erro no envio do e-mail!" }
 
 @app.post("/enviarEmail")
 async def enviar_email(obj: ObjetoNotificacao):
 
     try:
-        print(obj)
-
+      
         host = '10.0.0.241'
         port = 25
         email_de = 'qqtech-alunos@quero-quero.com.br'
-        #email_para = [f'{obj.email}']
 
         if obj.resposta == False:
             email_body = f"Efetuada solicitação (Id {obj.idSolicitacao}) de férias pelo colaborador {obj.nomeColaborador}. Possível ser acessado pelo link localhost:3001/solicitacao/{obj.idSolicitacao}."
@@ -79,7 +80,6 @@ async def enviar_email(obj: ObjetoNotificacao):
             email_body = f"O pedido de sua solicitação (id {obj.idSolicitacao}) foi {obj.analise}. Possível ser acessado pelo link localhost:3001/solicitacao/{obj.idSolicitacao}."
 
         msg = MIMEText(email_body, 'html')
-
         msg['Subject'] = "Solicitação QQFérias"
         msg['From'] = email_de
         msg['To'] = obj.email
@@ -91,8 +91,55 @@ async def enviar_email(obj: ObjetoNotificacao):
 
         return { "message": "E-mail enviado com sucesso!" }
     
-    except error:
+    except:
+
         return { "message": "Erro no envio do e-mail!" }
     
+@app.post("/enviarRelatorio")
+async def enviar_relatorio(obj: Request):
+
+    #try:
+
+        json = await obj.json()
+
+        print(json)
+
+        msg = MIMEMultipart()
+        host = '10.0.0.241'
+        port = 25
+        email_de = 'qqtech-alunos@quero-quero.com.br'
+
+        email_body = f"Solicitação do relatório efetuada, favor checar arquivo em anexo."
+
+        msg.attach(MIMEText(email_body, 'html'))
+
+        if 'colaboradores' in json:
+            dataFrame = pd.DataFrame.from_dict(json['data'])
+            listaColaboradores = dataFrame['colaboradores'].tolist()
+            df = pd.json_normalize(listaColaboradores)
+        else:
+            df = pd.DataFrame.from_dict(json['data'], orient='index').T
+        
+        df.to_csv('relatorio.csv', index=False)
+
+        with open('relatorio.csv', 'rb') as f:
+            anexo = MIMEApplication(f.read(), _subtype='csv')
+            anexo.add_header('content-disposition', 'attachment', filename='relatorio.csv')
+            msg.attach(anexo)
+
+        msg['Subject'] = "Solicitação Relatório QQFérias"
+        msg['From'] = email_de
+        msg['To'] = json['email']
+
+        s = smtplib.SMTP(host, port)
+        s.ehlo()
+        s.sendmail(email_de, json['email'], msg.as_string())
+        s.quit()
+
+     #   return { "message": "E-mail enviado com sucesso!" }
+    
+   # except:
+
+    #    return { "message": "Erro no envio do e-mail!" }
 
 #uvicorn main:app --reload -> rodar o server
